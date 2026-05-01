2ca2ed5dfc4ab57f38d26115b97cc69db019c33a
夜凌 <abcd1844314159@163.com>
2026-05-01 21:15:14 +0800

    快速幂

--- 基本算法/D-Raising Modulo Numbers.cpp
+++ 基本算法/D-Raising Modulo Numbers.cpp
@@ -3,13 +3,43 @@ using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
+ll quickPower(ll a,ll b,ll p){
+    ll res=1%p;
+
+    a%=p;
+    while(b){
+        if(b&1){
+            res=(res*a)%p;
+        }
+        a=a*a%p;
+        b>>=1;
+    }
+
+    return res%p;
+
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int Z;
+    cin>>Z;
+    while(Z--){
+        ll MOD;
+        cin>>MOD;
+        ll H;
+        cin>>H;
+        ll sum=0;
+        while(H--){
+            ll a,b;
+            cin>>a>>b;
+            sum=(sum+quickPower(a,b,MOD))%MOD;
+        }
+        cout<<sum<<endl;
+    }
+
 
     return 0;
 }
