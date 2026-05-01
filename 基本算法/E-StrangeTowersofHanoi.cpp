41a6d2f6240f634cc31232a95a0544bdcf8fe9e0
夜凌 <abcd1844314159@163.com>
2026-05-01 20:04:36 +0800

    添加汉诺塔变种题解

---
+++ 基本算法/E-StrangeTowersofHanoi.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
